f6544a97e0eb430170286cce41dc2df09255a261
TommySasio <TommySasio@gmail.com>
2024-11-25 17:13:32 -0500

    Add files via upload

---
+++ apptdeaionic/src/app/add-post/add-post.page.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post.model';
+import { ToastController, LoadingController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { NavController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Component({
+  selector: 'app-add-post',
+  templateUrl: './add-post.page.html',
+  styleUrls: ['./add-post.page.scss'],
+})
+export class AddPostPage implements OnInit {
+
+  post = {} as Post;
+
+  constructor(
+    private toastCtrl: ToastController,
+    private afAuth: AngularFireAuth,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController,
+    private firestore: AngularFirestore
+  ) {}  
+
+  ngOnInit() {}
+
+  async createPost(post: Post) {
+
+    if (this.formValidation()) {
+      let loader = await this.loadingCtrl.create({
+        message: "Espere un momento por favor..."
+      });
+      await loader.present();
+  
+      try {
+        await this.firestore.collection("posts").add(post);
+
+      } catch (e: any) {
+        let errorMessage = e.message || e.getLocalizedMessage();
+
+        this.showToast(errorMessage);
+      }
+  
+      await loader.dismiss();
+
+      this.navCtrl.navigateRoot("home");
+
+    }
+  }
+  
+  formValidation() {
+    if (!this.post.title) {
+      this.showToast("Ingrese un título");
+      return false;
+    }
+  
+    if (!this.post.details) {
+      this.showToast("Ingrese una descripción");
+      return false;
+    }
+  
+    return true;
+  }
+  
+  showToast(message: string) {
+    this.toastCtrl.create({
+      message: message,
+      duration: 5000 // Aumenta la duración del mensaje a 5 segundos
+    }).then(toastData => toastData.present());
+  }
+  
+
+}
